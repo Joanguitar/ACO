6e5b76362609725b577156404e5cc000313f0bb0
Joan Palacios <jopabel93@gmail.com>
2020-04-17 12:09:05 +0200

    packages

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Scatter } from 'react-chartjs-2';
+
+import {abs, range, pi, max} from 'mathjs';
+
+// reactstrap components
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardText,
+  CardTitle,
+  Label,
+  FormGroup,
+  Input,
+  Table,
+  Row,
+  Col
+} from "reactstrap";
+
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import Slider from '@material-ui/core/Slider';
+import 'katex/dist/katex.min.css';
+import { InlineMath, BlockMath } from 'react-katex';
 
 class App extends React.Component {
-  render() {}
+  render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
